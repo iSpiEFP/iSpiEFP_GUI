f970462a0f7c3a6b8a6fd85740362838e685b5d0
rderue <rderue@purdue.edu>
2020-05-15 08:22:37 -0400

    Added files selectRMSD.fxml and selectRMSDController.java to git repo

---
+++ iSpiEFP/src/org/ispiefp/app/libEFP/SelectRMSDController.java
@@ -0,0 +1,18 @@
+package org.ispiefp.app.libEFP;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class SelectRMSDController {
+
+    @FXML
+    ListView<String> libraryFragments;
+
+    public SelectRMSDController(){
+        super();
+    }
+
+    public void initialize() {
+
+    }
+}
