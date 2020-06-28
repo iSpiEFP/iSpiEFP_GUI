15f86d4c4024231c36661d7e9212984eadd65651
jmckeev <jmckeev@purdue.edu>
2020-06-28 18:48:21 -0400

    Adding LibEFP to CSV file functionality

--- iSpiEFP/src/org/ispiefp/app/EFPFileRetriever/LibEFPtoCSV.java
+++ iSpiEFP/src/org/ispiefp/app/EFPFileRetriever/LibEFPtoCSV.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class LibEFPtoCSV {
     //private final String[] files = {"opt_1.out", "pbc_1.out", "w6b2_energy.out"};
     private final int ENERGIES = 6; // the number of different energy calculations, excluding total
+    private final int WHITESPACE = 34;
     private final int AUXILIARY = 3; // the auxiliary columns that may have additional information
 
     /* This method takes as a parameter a file name and gets the necessary information to create a string to write to
@@ -14,67 +15,55 @@ public class LibEFPtoCSV {
         "kinetic_energy,invariant,temperature\n" (molecular dynamics)
      */
 
+    public LibEFPtoCSV() {
+        System.out.println("Preparing to convert files");
+    }
+
     public String getCSVString(String fileName) {
         String csvString = new String();
-       File file = new File(fileName);
-       int geometries = 0;
-       try {
-           FileReader fileReader = new FileReader(file);
-           BufferedReader bufferedReader = new BufferedReader(fileReader);
-           String line;
-           while (true) {
-               line = bufferedReader.readLine();
-               if (line == null) {
-                   break;
-               }
+        File file = new File(fileName);
+        int geometries = 0;
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while (true) {
+                line = bufferedReader.readLine();
+                if (line == null) {
+                    break;
+                }
 
-               /* Get the energy results once we reach the key line */
+                /* Get the energy results once we reach the key line */
 
-               if (line.contains("ENERGY COMPONENTS")) {
+                if (line.contains("ENERGY COMPONENTS")) {
 
-                   /* add to the first column the iteration and a comma */
+                    /* add to the first column the iteration and a comma */
 
-                   csvString += geometries;
-                   csvString += ",";
-                   bufferedReader.readLine();    // read the empty line
+                    csvString += geometries;
+                    geometries++;
+                    csvString += ",";
+                    bufferedReader.readLine();    // read the empty line
+
                    /* repeat the following process to get the energy for each component (6 times)
                       Electrostatic, polarization, dispersion, exchange repulsion, point charges, and charge penetration
                     */
 
-                   for (int i = 0; i < this.ENERGIES; i++) {
-                       line = bufferedReader.readLine();
-                       String[] components = line.split(" ");
-                       for (int j = 0; j < components.length; i++) {
-                           try {
-                               Integer.parseInt(components[j]);
-                               csvString += components[j];
-                               csvString += ",";
-                           } catch (NumberFormatException e) {
-                               //continue;
-                           }
-                       }
-                   }
-                   bufferedReader.readLine(); // read the empty line
+                    for (int i = 0; i < this.ENERGIES; i++) {
+                        csvString += getLineInformation(bufferedReader);
+                        csvString += ",";
+                    }
+                    bufferedReader.readLine(); // read the empty line
 
-                   /* get the total energy */
+                    /* get the total energy */
 
-                   line = bufferedReader.readLine();
-                   String[] components = line.split(" ");
-                   for (int i = 0; i < components.length; i++) {
-                       try {
-                           Integer.parseInt(components[i]);
-                           csvString += components[i];
-                           csvString += ",";
-                       } catch (NumberFormatException e) {
-                           //e.printStackTrace();
-                       }
-                   }
+                    csvString += getLineInformation(bufferedReader);
+                    csvString += ",";
 
-                   /* read the two blank lines */
+                    /* read the two blank lines */
 
-                   bufferedReader.readLine();
-                   bufferedReader.readLine();
-                   line = bufferedReader.readLine();
+                    bufferedReader.readLine();
+                    bufferedReader.readLine();
+                    line = bufferedReader.readLine();
 
                    /* Depending on the following lines, get the correct information and signify what the columns
                       correspond to:
@@ -83,74 +72,71 @@ public class LibEFPtoCSV {
                       2 = molecular dynamics
                       These values are the identifier descibed above
                     */
-                   if (line.contains("ENERGY CHANGE")) {
-                       csvString += "1,";
-                       for (int i = 0; i < AUXILIARY; i++) {
-                            line = bufferedReader.readLine();
-                            String[] auxiliaryColumns = line.split(" ");
 
-                            /* Get the information from the auxiliary columns and add commas, except for the last,
-                               as we need to add a newline character manually
-                             */
-                           for (int j = 0; j < auxiliaryColumns.length - 1; j++) {
-                               try {
-                                   Integer.parseInt(auxiliaryColumns[j]);
-                                   csvString += auxiliaryColumns[j];
-                                   csvString += ",";
-                               } catch (NumberFormatException e) {
-                                   //e.printStackTrace();
-                               }
-                           }
-                           line = bufferedReader.readLine();
-                           String[] finalColumn = line.split(" ");
-                           for (int j = 0; j < finalColumn.length; j++) {
-                               try {
-                                   Integer.parseInt(finalColumn[j]);
-                                   csvString += finalColumn[j];
-                                   csvString += "\n";
-                               } catch (NumberFormatException e) {
-                                   //e.printStackTrace();
-                               }
-                           }
-                       }
-                   } else if (line.contains("KINETIC ENERGY")) {
+                    if (line.contains("ENERGY CHANGE")) {
+                        csvString += "1,";
+
+                        /* Get the information from the first auxiliary column, which is the line
+                           we have just read. Skip the beginning whitespace.
+                         */
+
+                        for (int i = this.WHITESPACE; i < line.length(); i++) {
+                            if (line.charAt(i) != ' ') {
+                                csvString += line.charAt(i);
+                            }
+                        }
+                        csvString += ",";
+
+                        /* Get the information for the second auxiliary column as normal */
+
+                        csvString += getLineInformation(bufferedReader);
+                        csvString += ",";
+
+                        /* Get the information for the last auxiliary column and add a newline */
+
+                        csvString += getLineInformation(bufferedReader);
+                        csvString += "\n";
+                    } else if (line.contains("KINETIC ENERGY")) {
                         csvString += "2,";
-                       for (int i = 0; i < AUXILIARY; i++) {
-                           line = bufferedReader.readLine();
-                           String[] auxiliaryColumns = line.split(" ");
 
-                            /* Get the information from the auxiliary columns and add commas, except for the last,
-                               as we need to add a newline character manually
-                             */
-                           for (int j = 0; j < auxiliaryColumns.length - 1; j++) {
-                               try {
-                                   Integer.parseInt(auxiliaryColumns[j]);
-                                   csvString += auxiliaryColumns[j];
-                                   csvString += ",";
-                               } catch (NumberFormatException e) {
-                                   //e.printStackTrace();
-                               }
-                           }
-                           line = bufferedReader.readLine();
-                           String[] finalColumn = line.split(" ");
-                           for (int j = 0; j < finalColumn.length; j++) {
-                               try {
-                                   Integer.parseInt(finalColumn[j]);
-                                   csvString += finalColumn[j];
-                                   csvString += "\n";
-                               } catch (NumberFormatException e) {
-                                   //e.printStackTrace();
-                               }
-                           }
-                       }
-                   } else {
+                        /* Get the information from the first auxiliary column, which is the line
+                           we have just read. Skip the beginning whitespace.
+                         */
+
+                        for (int i = this.WHITESPACE; i < line.length(); i++) {
+                            if (line.charAt(i) != ' ') {
+                                csvString += line.charAt(i);
+                            }
+                        }
+                        csvString += ",";
+
+                        /* Get the information for the second auxiliary column as normal */
+
+                        csvString += getLineInformation(bufferedReader);
+                        csvString += ",";
+
+                        /* Get the information for the last auxiliary column and add a newline */
+
+                        csvString += getLineInformation(bufferedReader);
+                        csvString += "\n";
+                    } else {
                         csvString += "0,-1,-1,-1\n";
-                   }
-               }
-           }
-       } catch (IOException e) {
-           e.printStackTrace();
-       }
-       return csvString;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return csvString;
+    }
+
+    private String getLineInformation(BufferedReader bufferedReader) throws IOException {
+        bufferedReader.skip(this.WHITESPACE);
+        String line = bufferedReader.readLine();
+        if (line.charAt(0) == ' ') {
+            return line.substring(1);
+        } else {
+            return line;
+        }
     }
 }
