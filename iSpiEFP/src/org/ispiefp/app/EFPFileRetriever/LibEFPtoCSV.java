91b72399580bcc1c4824f12d8a30e4db6ee25439
jmckeev <jmckeev@purdue.edu>
2020-07-02 12:44:50 -0400

    Adding LibEFP to CSV file functionality

--- iSpiEFP/src/org/ispiefp/app/EFPFileRetriever/LibEFPtoCSV.java
+++ iSpiEFP/src/org/ispiefp/app/EFPFileRetriever/LibEFPtoCSV.java
@@ -3,11 +3,17 @@ package org.ispiefp.app.EFPFileRetriever;
 import java.io.*;
 
 public class LibEFPtoCSV {
-    //private final String[] files = {"opt_1.out", "pbc_1.out", "w6b2_energy.out"};
     private final int ENERGIES = 6; // the number of different energy calculations, excluding total
     private final int TOTAL_ENERGY_WHITESPACE = 34; // the leading whitespace in a total energy file
     private final int PAIRWISE_WHITESPACE = 44; // the leading whitespace in a pairwise energy file
-    //private final int AUXILIARY = 3; // the auxiliary columns that may have additional information
+    private final int TOTAL_INDEX = 0;
+    private final int PAIRWISE_INDEX = 1;
+
+    private enum Type {
+        REGULAR,
+        OPTIMIZATION,
+        MOLECULAR
+    }
 
     /* This method takes as a parameter a file name and gets the necessary information to create a string to write to
        a CSV file. The string is in the format "geometry_number,electrostatic,polarization,dispersion,
@@ -16,14 +22,19 @@ public class LibEFPtoCSV {
         "kinetic_energy,invariant,temperature\n" (molecular dynamics)
      */
 
+
     public LibEFPtoCSV() {
         System.out.println("Preparing to convert files");
     }
 
-    public String getCSVString(String fileName) {
-        String csvString = new String();
+    public String[] getCSVString(String fileName) {
+        String[] csvString = new String[2];
+        boolean totalEntered = false;
+        boolean pairwiseEntered = false;
         File file = new File(fileName);
         int geometries = 0;
+        int fragment = 1;
+        Type type = Type.REGULAR;
         try {
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -39,33 +50,43 @@ public class LibEFPtoCSV {
                  */
 
                 if (line.contains("PAIRWISE ENERGY COMPONENTS")) {
-                    csvString += "0,";
-                    csvString += geometries;
-                    geometries ++;
-                    csvString += ",";
+                    if (!pairwiseEntered) {
+                        csvString[PAIRWISE_INDEX] = new String();
+                        csvString[PAIRWISE_INDEX] += "Ligand #,Fragment #,Electrostatic,Polarization,Dispersion,Exchange-repulsion," +
+                                "Charge-penetration,Total\n";
+                        pairwiseEntered = true;
+                    }
+                    csvString[PAIRWISE_INDEX] += "0,";
+                    csvString[PAIRWISE_INDEX] += fragment;
+                    fragment++;
+                    csvString[PAIRWISE_INDEX] += ",";
                     for (int i = 0; i < this.ENERGIES; i++) {
-                        csvString += getLineInformation(bufferedReader, this.PAIRWISE_WHITESPACE);
+                        csvString[PAIRWISE_INDEX] += getLineInformation(bufferedReader, this.PAIRWISE_WHITESPACE);
                         if (i != this.ENERGIES - 1) {
-                            csvString += ",";
+                            csvString[PAIRWISE_INDEX] += ",";
                         }
                     }
-                    csvString += "\n";
+                    csvString[PAIRWISE_INDEX] += "\n";
                 } else if (line.contains("LATTICE ENERGY COMPONENTS")) {
-                    csvString += "0,-1,";
+                    csvString[PAIRWISE_INDEX] += "0,-1,";
                     for (int i = 0; i < this.ENERGIES; i++) {
-                        csvString += getLineInformation(bufferedReader, this.PAIRWISE_WHITESPACE);
+                        csvString[PAIRWISE_INDEX] += getLineInformation(bufferedReader, this.PAIRWISE_WHITESPACE);
                         if (i != this.ENERGIES - 1) {
-                            csvString += ",";
+                            csvString[PAIRWISE_INDEX] += ",";
                         }
                     }
-                    csvString += "\n";
+                    csvString[PAIRWISE_INDEX] += "\n";
                 } else if (line.contains("ENERGY COMPONENTS")) {
+                    if (!totalEntered) {
+                        csvString[TOTAL_INDEX] = new String();
+                        totalEntered = true;
+                    }
 
                     /* add to the first column the iteration and a comma */
 
-                    csvString += geometries;
+                    csvString[TOTAL_INDEX] += geometries;
                     geometries++;
-                    csvString += ",";
+                    csvString[TOTAL_INDEX] += ",";
                     bufferedReader.readLine();    // read the empty line
 
                    /* repeat the following process to get the energy for each component (6 times)
@@ -73,80 +94,82 @@ public class LibEFPtoCSV {
                     */
 
                     for (int i = 0; i < this.ENERGIES; i++) {
-                        csvString += getLineInformation(bufferedReader, this.TOTAL_ENERGY_WHITESPACE);
-                        csvString += ",";
+                        csvString[TOTAL_INDEX] += getLineInformation(bufferedReader, this.TOTAL_ENERGY_WHITESPACE);
+                        csvString[TOTAL_INDEX] += ",";
                     }
                     bufferedReader.readLine(); // read the empty line
 
                     /* get the total energy */
 
-                    csvString += getLineInformation(bufferedReader, this.TOTAL_ENERGY_WHITESPACE);
-                    csvString += ",";
+                    csvString[TOTAL_INDEX] += getLineInformation(bufferedReader, this.TOTAL_ENERGY_WHITESPACE);
 
                     /* read the two blank lines */
 
                     bufferedReader.readLine();
                     bufferedReader.readLine();
                     line = bufferedReader.readLine();
-
-                   /* Depending on the following lines, get the correct information and signify what the columns
-                      correspond to:
-                      0 = no additional information
-                      1 = geometry optimization
-                      2 = molecular dynamics
-                      These values are the identifier descibed above
-                    */
-
                     if (line.contains("ENERGY CHANGE")) {
-                        csvString += "1,";
+                        type = Type.OPTIMIZATION;
 
                         /* Get the information from the first auxiliary column, which is the line
-                           we have just read. Skip the beginning whitespace.
+                           we have just read. Add the comma beforehand. Skip the beginning whitespace.
                          */
 
+                        csvString[TOTAL_INDEX] += ",";
                         for (int i = this.TOTAL_ENERGY_WHITESPACE; i < line.length(); i++) {
                             if (line.charAt(i) != ' ') {
-                                csvString += line.charAt(i);
+                                csvString[TOTAL_INDEX] += line.charAt(i);
                             }
                         }
-                        csvString += ",";
+                        csvString[TOTAL_INDEX] += ",";
 
                         /* Get the information for the second auxiliary column as normal */
 
-                        csvString += getLineInformation(bufferedReader, this.TOTAL_ENERGY_WHITESPACE);
-                        csvString += ",";
+                        csvString[TOTAL_INDEX] += getLineInformation(bufferedReader, this.TOTAL_ENERGY_WHITESPACE);
+                        csvString[TOTAL_INDEX] += ",";
 
                         /* Get the information for the last auxiliary column and add a newline */
 
-                        csvString += getLineInformation(bufferedReader, this.TOTAL_ENERGY_WHITESPACE);
-                        csvString += "\n";
+                        csvString[TOTAL_INDEX] += getLineInformation(bufferedReader, this.TOTAL_ENERGY_WHITESPACE);
+                        csvString[TOTAL_INDEX] += "\n";
                     } else if (line.contains("KINETIC ENERGY")) {
-                        csvString += "2,";
+                        type = Type.MOLECULAR;
 
                         /* Get the information from the first auxiliary column, which is the line
-                           we have just read. Skip the beginning whitespace.
+                           we have just read. Add the comma beforehand. Skip the beginning whitespace.
                          */
 
+                        csvString[TOTAL_INDEX] += ",";
                         for (int i = this.TOTAL_ENERGY_WHITESPACE; i < line.length(); i++) {
                             if (line.charAt(i) != ' ') {
-                                csvString += line.charAt(i);
+                                csvString[TOTAL_INDEX] += line.charAt(i);
                             }
                         }
-                        csvString += ",";
+                        csvString[TOTAL_INDEX] += ",";
 
                         /* Get the information for the second auxiliary column as normal */
 
-                        csvString += getLineInformation(bufferedReader, this.TOTAL_ENERGY_WHITESPACE);
-                        csvString += ",";
+                        csvString[TOTAL_INDEX] += getLineInformation(bufferedReader, this.TOTAL_ENERGY_WHITESPACE);
+                        csvString[TOTAL_INDEX] += ",";
 
                         /* Get the information for the last auxiliary column and add a newline */
 
-                        csvString += getLineInformation(bufferedReader, this.TOTAL_ENERGY_WHITESPACE);
-                        csvString += "\n";
+                        csvString[TOTAL_INDEX] += getLineInformation(bufferedReader, this.TOTAL_ENERGY_WHITESPACE);
+                        csvString[TOTAL_INDEX] += "\n";
                     } else {
-                        csvString += "0,-1,-1,-1\n";
+                        csvString[TOTAL_INDEX] += "\n";
                     }
                 }
+            }
+            if (type == Type.REGULAR) {
+                csvString[TOTAL_INDEX] = "Geometry #,Electrostatic,Polarization,Dispersion,Exchange-repusion,Point charges," +
+                        "Charge-penetration,Total\n" + csvString[TOTAL_INDEX];
+            } else if (type == Type.OPTIMIZATION) {
+                csvString[TOTAL_INDEX] = "Geometry #,Electrostatic,Polarization,Dispersion,Exchange-repusion,Point charges," +
+                        "Charge-penetration,Total,Energy Change,RMS Gradient,Maximum Gradient\n" + csvString[TOTAL_INDEX];
+            } else {
+                csvString[TOTAL_INDEX] = "Geometry #,Electrostatic,Polarization,Dispersion,Exchange-repusion,Point charges," +
+                        "Charge-penetration,Total,Kinetic Energy,Invariant,Temperature (K)\n" + csvString[TOTAL_INDEX];
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -162,20 +185,5 @@ public class LibEFPtoCSV {
         } else {
             return line;
         }
-//        String line = bufferedReader.readLine();
-//        String[] components = line.split(" ");
-//        String csvLine = new String();
-//        for (int i = 0; i < components.length; i++) {
-//            try {
-//                if (components[i].compareTo("") != 0) {
-//                    Double.parseDouble(components[i]);
-//                    line += Double.parseDouble(components[i]);
-//                    line += ",";
-//                }
-//            } catch (NumberFormatException nfe) {
-//
-//            }
-//        }
-//        return csvLine;
     }
 }
